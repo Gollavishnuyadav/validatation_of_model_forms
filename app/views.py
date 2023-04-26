353724cc98be03484c6c66987b3d38b592eecca8
Gollavishnuyadav <gollavishnuyadav1@gmail.com>
2023-04-26 19:03:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from app.forms import *
+def insert_topic(request):
+    TF=TopicForm()
+    d={'TF':TF}
+    if request.method=='POST':
+        TFD=TopicForm(request.POST)
+        if TFD.is_valid():
+            TFD.save()
+            return HttpResponse('Topic is Inserted')
+        else:
+            return HttpResponse('data is not valid')
+    return render(request,'topic.html',d)
